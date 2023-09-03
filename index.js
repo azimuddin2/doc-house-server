6710734383ac98d79fcf9adbdb3627a0d15b4694
azimuddin2 <mohammadazimuddin274@gmail.com>
2023-09-03 14:22:49 +0600

    our services operation

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
-
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// database connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.snyiliq.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run(){
+    try{
+        const ourServicesCollection = client.db('docHouse').collection('ourService');
+    
+        app.get('/our-services', async(req, res) => {
+            const query = {};
+            const services = await ourServicesCollection.find(query).toArray();
+            res.send(services);
+        });
+
+    }
+    finally{}
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
