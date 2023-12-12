cab13a2684d457ab27ff5ba1a5624a6506e3fa99
azimuddin2 <mohammadazimuddin274@gmail.com>
2023-12-12 07:34:12 +0000

    understand admin role task

--- index.js
+++ index.js
@@ -52,12 +52,23 @@ async function run() {
             res.send({ token });
         });
 
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+
+            if (user?.role !== 'admin') {
+                return res.status(403).send({ error: true, message: 'forbidden message' })
+            }
+            next();
+        };
+
         // users related api
-        app.get('/users', async (req, res) => {
+        app.get('/users', verifyJWT, verifyAdmin, async (req, res) => {
             const query = {};
             const users = await usersCollection.find(query).toArray();
             res.send(users);
-        })
+        });
 
         app.post('/users', async (req, res) => {
             const user = req.body;
@@ -71,6 +82,20 @@ async function run() {
             const result = await usersCollection.insertOne(user);
             res.send(result);
         });
+
+        app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            const decodedEmail = req.decoded.email;
+
+            if (decodedEmail !== email) {
+                res.send({ admin: false });
+            }
+
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            const result = { admin: user?.role === 'admin' };
+            res.send(result);
+        })
 
         app.patch('/users/admin/:id', async (req, res) => {
             const id = req.params.id;
